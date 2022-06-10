9435e2a54617296154f2caf18aba4061c639b408
jamal <lordareello@gmail.com>
2022-06-10 19:13:28 +0100

    added router paths

--- app/api_v1/api.py
+++ app/api_v1/api.py
@@ -1,7 +1,23 @@
 from fastapi import APIRouter
 
-
+from .endpoints import (
+    shipping,
+    products,
+    orders,
+    invoices,
+    collections,
+    category
+)
 
 
 router = APIRouter()
+
+router.include_router(shipping.router, prefix='/shipping', tags=['Shipping'])
+router.include_router(products.router, prefix='/products', tags=['Products'])
+router.include_router(orders.router, prefix='/orders', tags=['Orders'])
+router.include_router(products.router, prefix='/products', tags=['Products'])
+router.include_router(orders.router, prefix='/orders', tags=['Orders'])
+router.include_router(invoices.router, prefix='/invoices', tags=['Invoices'])
+router.include_router(collections.router, prefix='/collections', tags=['Collections'])
+router.include_router(category.router, prefix='/categories', tags=['Categories'])
 
