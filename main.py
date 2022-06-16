c431d2e9bbce995329ded1cf1e68b1093775262f
jamal <lordareello@gmail.com>
2022-06-16 21:50:56 +0100

    added root

--- main.py
+++ main.py
@@ -51,7 +51,9 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
 
